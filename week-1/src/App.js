d17aba727f0ca2429dc25bc6309eb75ecdd930a8
JiHoon <skyy630@naver.com>
2021-09-21 02:50:19 +0900

    9.20

---
+++ week-1/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+
+// let hoon = "jihoon"
+
+function App() {
+  const divStyle = {
+    width:'200px',
+    padding:'20px', 
+    border:'solid skyblue 3px', 
+    borderRadius:'10px',
+    margin: 'auto'
+  }
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
