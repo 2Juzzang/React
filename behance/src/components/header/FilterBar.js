0724842c1f8a53c757e28d40ad516addf9908418
JiHoon <skyy630@naver.com>
2022-04-12 17:34:23 +0900

    header 완성

--- behance/src/components/header/FilterBar.js
+++ behance/src/components/header/FilterBar.js
@@ -1,7 +1,51 @@
 import React from 'react';
 
 const FilterBar = () => {
-  return <div className='h-12 py-10 bg-black'></div>;
+  return (
+    <div className='flex h-18 p-6 bg-white'>
+      <div className='border rounded-md w-48 p-2 mr-4'>
+        <select className='w-44 text-sm font-bold' id='dropdown'>
+          <option key='creative'>크리에이티브 분야</option>
+        </select>
+      </div>
+
+      <div className='border rounded-md w-auto p-2 mr-4'>
+        <select className='w-auto text-sm font-bold' id='dropdown'>
+          <option key='tool'>도구</option>
+        </select>
+      </div>
+
+      <div className='border rounded-md w-auto p-2 mr-4'>
+        <select className='w-auto text-sm font-bold' id='dropdown'>
+          <option key='color'>색상</option>
+        </select>
+      </div>
+
+      <div className='border rounded-md w-auto p-2 mr-4'>
+        <select className='w-auto text-sm font-bold' id='dropdown'>
+          <option key='location'>위치</option>
+        </select>
+      </div>
+
+      <div className='border rounded-md w-auto p-2 mr-4'>
+        <select className='w-auto text-sm font-bold' id='dropdown'>
+          <option key='school'>학교</option>
+        </select>
+      </div>
+
+      <div className='border rounded-md w-auto p-2 mr-4'>
+        <select className='w-auto text-sm font-bold' id='dropdown'>
+          <option key='color'>소스 파일</option>
+        </select>
+      </div>
+
+      <div className='border rounded-md w-auto p-2 mr-4'>
+        <select className='w-auto text-sm font-bold' id='dropdown'>
+          <option key='desc'>구독</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default FilterBar;
